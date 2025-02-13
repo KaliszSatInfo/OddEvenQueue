57d867c757e1b098ec442be3af1b2740d68175de
KaliszSatInfo <jan.kalisz007@gmail.com>
2025-02-13 12:10:08 +0100

    Finished, but DeepSeek

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,131 @@
 interface IQueue {
+    void add(int value);
+    int remove();
+    boolean isEmpty();
 }
 
 class Node {
-    int data;
-    Node next;
-    Node prev;
+    private int data;
+    private Node next;
+    private Node prev;
 
     public Node(int data) {
         this.data = data;
         this.next = null;
         this.prev = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
 }
 
 class Queue implements IQueue {
-    protected Node head;
-    protected Node tail;
+    private Node head;
+    private Node tail;
 
     public Queue() {
         this.head = null;
         this.tail = null;
     }
 
-    public int remove() {
-        if (head == null) {
-            throw new IllegalStateException("Queue is empty");
+    protected Node getHead() {
+        return head;
+    }
+
+    protected void setHead(Node head) {
+        this.head = head;
+    }
+
+    protected Node getTail() {
+        return tail;
+    }
+
+    protected void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    @Override
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.setNext(newNode);
+            newNode.setPrev(tail);
         }
-        int value = head.data;
-        head = head.next;
+        tail = newNode;
+    }
+
+    @Override
+    public int remove() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty. Cannot remove from an empty queue.");
+        }
+        int value = head.getData();
+        head = head.getNext();
         if (head != null) {
-            head.prev = null;
+            head.setPrev(null);
         } else {
             tail = null;
         }
         return value;
     }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
 
 class PriorityQueue extends Queue {
-    public void insert(int value) {
+    @Override
+    public void add(int value) {
         Node newNode = new Node(value);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
+        if (isEmpty()) {
+            super.add(value);
             return;
         }
 
         if (value % 2 != 0) {
-            Node current = head;
+            Node current = getHead();
             Node lastOdd = null;
-            while (current != null && current.data % 2 != 0) {
+            while (current != null && current.getData() % 2 != 0) {
                 lastOdd = current;
-                current = current.next;
+                current = current.getNext();
             }
             if (lastOdd == null) {
-                newNode.next = head;
-                head.prev = newNode;
-                head = newNode;
+                newNode.setNext(getHead());
+                getHead().setPrev(newNode);
+                setHead(newNode);
             } else {
-                newNode.next = lastOdd.next;
-                newNode.prev = lastOdd;
-                lastOdd.next = newNode;
-                if (newNode.next != null) {
-                    newNode.next.prev = newNode;
+                newNode.setNext(lastOdd.getNext());
+                newNode.setPrev(lastOdd);
+                lastOdd.setNext(newNode);
+                if (newNode.getNext() != null) {
+                    newNode.getNext().setPrev(newNode);
                 } else {
-                    tail = newNode;
+                    setTail(newNode);
                 }
             }
         } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
+            super.add(value);
         }
     }
 }
@@ -78,21 +133,17 @@ class PriorityQueue extends Queue {
 public class Main {
     public static void main(String[] args) {
         PriorityQueue queue = new PriorityQueue();
-        queue.insert(5);
-        queue.insert(6);
+        queue.add(5);
+        queue.add(6);
         queue.remove();
-        queue.insert(7);
-        queue.insert(3);
-        queue.insert(10);
+        queue.add(7);
+        queue.add(3);
+        queue.add(10);
         queue.remove();
         queue.remove();
 
-        while (true) {
-            try {
-                System.out.print(queue.remove() + " ");
-            } catch (IllegalStateException e) {
-                break;
-            }
+        while (!queue.isEmpty()) {
+            System.out.print(queue.remove() + " ");
         }
     }
 }
